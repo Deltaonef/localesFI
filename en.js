a9053bf217359a1466484d1c52583bb8bf38fb29
Deltaonef <71275992+Deltaonef@users.noreply.github.com>
2022-03-17 08:22:29 +0200

    Create en.js

---
+++ en.js
@@ -0,0 +1,383 @@
+Locales["en"] = {
+    /////////////
+    // General //
+    /////////////
+    "months": [
+        "Tammikuu",
+        "Helmikuu", 
+        "Maaliskuu", 
+        "Huhtikuu", 
+        "Toukokuu", 
+        "Kesäkuu", 
+        "Heinäkuu", 
+        "Elokuu", 
+        "Syyskuu", 
+        "Lokakuu", 
+        "Marraskuu", 
+        "Joulukuu"
+    ], // Do not change the order the months are laid out.
+    "weekdays": [
+        "Sunnuntai",
+        "Maanantai", 
+        "Tiistai", 
+        "Keskiviikko", 
+        "Torstai", 
+        "Perjantai", 
+        "Lauantai"
+    ], // Do not change the order the days are laid out.
+    "today": "Tänään",
+    "yesterday": "Eilen",
+    "justnow": "Juuri Nytten",
+    "unknown": "Tuntematon",
+    "error": "Virhe",
+    "foundresults": "Löytyi {0} tulosta", // {0} is result count
+    "searchquery": 'haulle "{0}"', // {0} is search query
+    // App store
+    "get": "LATAA",
+    "remove": "POISTA",
+    // Phone app
+    "calling": "Soitetaan..",
+    "connecting": "Yhdistetään..",
+    "callended": "Puhelu päättyi",
+    "cancelled": "Peruutettu",
+    "privatenumber": "Yksityinen numero",
+    // Message app
+    "person": "Henkilö",
+    "people": "Ryhmä",
+    "member": "Jäsen",
+    "members": "Jäsenet",
+    "location": "🗺️ Kartta",
+    "attachment": "Liite #{0}", // {0} is the attachment ID
+    "attached_image": "(Liitetty kuva)",
+    "left_contacts": "Voit valita {0} muuta kontaktia", // {0} is the remaining contact count
+    "max_contacts": "Voit valita yhteensä {0} kontaktia!", // {0} is the allowed contact count
+    "new_group_message": "<strong>{0}</strong> ryhmässä <strong>{1}</strong>:<br>{2}", // {0} is the sender name/number, {1} is the group name, {2} is the content
+    // Bank app
+    "iban": "IBAN: {0}", // {0} is player bank account number
+    "cardname": "NORDEA",
+    "cardexpiry": "VIIMEINEN PV",
+    "invoicedescription": "Määrä: <strong>{0} €</strong>", // {0} is the invoice amount
+    "requestdescription": "Pyytö henkilöltä <strong>{0} [{1}]</strong><br>Syy: <strong>{2}</strong>", // {0} is player name, {1} is player ID, {2} is purpose (reason)
+    // Darkchat app
+    "you": "(Sinä)",
+    "new_dark_group_message": "<strong>{0}</strong> ryhmässä <strong>{1}</strong>:<br>{2}", // {0} is the sender name/number, {1} is the group name, {2} is the content
+    // Settings app
+    "playerinfo": "<strong>{0}</strong><br>ID: <strong>{1}</strong>", // {0} is player number, {1} is player ID
+    // Notes app
+    "new_note": "Uusi muistiinpano",
+    // Context menu
+    "edit": "Muokkaa",
+    "report": "Ilmianna",
+    "delete": "Poista",
+    "reply": "Vastaa",
+    "addcontact": "Lisää Kontakti",
+    "mutechat": "Mykistä keskustelu",
+    "unmutechat": "Poista keskustelun mykistys",
+    "mutegroup": "Mykistä ryhmä",
+    "unmutegroup": "Poista ryhmän mykistys",
+    "markasread": "Merkitse luetuksi",
+    "leavegroup": "Poistu ryhmästä",
+    "deletegroup": "Poista ryhmä",
+    "copyinvite": "Kopioi kutsu",
+    // Camera app
+    "uploadimage": 'Paina <span style="color: #FDBB13">julkaistaksesi</span> kuva!',
+    "uploadingimage": "Julkaistaan..",
+    "failedupload": "Julkaisu epäonnistui!",
+    // Choose photos
+    "totalphotos": "{0} kuvaa",
+    // AirDrop
+    "shared_contact": '<span style="font-family:PoppinsRegular;color: white;">{0} [{1}]</span> haluaa jakaa kontaktin kanssasi',
+
+    ///////////////////
+    // Notifications //
+    ///////////////////
+    "notifications": {
+        // Main
+        "system": "JÄRJESTELMÄ",
+        "error": "Virhe!",
+        "wrongimageurl": "Virheellinen kuvan linkki!",
+        "imagenotavailable": "Kuvan linkki ei ole saatavilla!",
+        "locationsent": "Sijainti lähetetty!",
+        "waypointset": "Sijainti waypointtiin asetettu!",
+        "namenotfilled": "Sinun on täytettävä nimi!",
+        "numbernotfilled": "Sinun on täytettävä numero!",
+        "messagenotfilled": "Sinun on täytettävä viesti!",
+        "locationcooldown": "Lähetät sijainteja liian lujaa!",
+        // Camera app
+        "imageuploadtoooften": "Lähetät kuvia liian usein! Odota muutama sekunti!",
+        "imageurlcopied": "Kuvan linkki kopioitu!",
+        "cantrotateinportrait": "Ei voi kääntää kameraa selfie kuvassa!",
+        // Contacts app
+        "contactalreadyexists": "Sinulla on jo kontakti tällä numerolla!",
+        "cannoteditcontact": "Ett voi muokata tätä kontaktia!",
+        "cannotcallcontact": "Ett voi soittaa kyseiselle kontaktille!",
+        "cannotdeletecontact": "Ett voi poistaa kyseistä kontaktia!",
+        "cannotcreatecontactyourself": "Ett voi tehdä itsestäsi kontaktia!",
+        "cannotsetnumbertoyourown": "Ett voi asettaa puhelin numeroa omaksesi!",
+        "sharingcooldown": "Ett voi jakaa kontakteja näin nopeaan!",
+        "sharedcontact": "Jaoit kontaktin lähimmälle henkilölle!",
+        // Phone app
+        "usernotavailable": "Käyttäjä ei ole tavoitettavissa!",
+        "cannotcallyourself": "Ett voi soittaa itsellesi!",
+        "alreadyincall": "Olet jo puhelussa!",
+        // Message app
+        "airplanecantsend": "Ett voi lähettää viestejä lentokonetilassa!",
+        "airplanecantcreategroup": "Ett voi luoda ryhmiä lentokonetilassa!",
+        "airplanecanteditgroup": "Ett voi muokata ryhmiä lentokonetilassa!",
+        "airplanecantlocation": "Ett voi jakaa sijaintiasi lentokonetilassas!",
+        "airplanecantcall": "Ett voi soittaa lentokonetilassa!",
+        "cannotsendtoyourself": "Ett voi lähettäÂ viestejä itsellesi!",
+        "cannotsendlocationtoyourself": "Ett voi lähettää sijaintia itsellesi!",
+        "cantmessageprivatenumber": "Ett voi laittaa viestiä yksityiselle numerolle!",
+        "toomanyattachments": "Voit liittää vain {0} kuvaa!", // {0} is the configured limit of attachment limit
+        "selectgroupmembers": "Sinun on valittava ainakin 1 henkilö!",
+        "namenotfilled": "Sinun on täytettävä ryhmän nimi!",
+        "max_contacts": "Voit valita vain {0} kontaktia yhteensä!", // {0} is the allowed contact count
+        "cantcreategroupsoff": "Ett voi luoda ryhmiä jos ryhmäkeskustelut eivät ole päällä!",
+        "cantopengroupsoff": "Ett voi avata ryhmiä, jos ryhmäkeskustelut on pois päältä!",
+        "addedtogroup": "Sinut lisättiin ryhmään nimeltä <strong>{0}</strong>!",
+        // Dark chat app
+        "airplanecantjoindarkgroups": "Cannot join groups with airplane mode on!",
+        "airplanecantcreatedarkgroup": "Cannot create groups with airplane mode on!",
+        "airplanecanteditdarkgroup": "Cannot edit groups with airplane mode on!",
+        "darknicknamenotfilled": "You need to fill in your nickname!",
+        "darkgroupmembersoutofrange": "Group members limit out of range!",
+        "darkgroupnamenotfilled": "You need to fill in the group name!",
+        "copiedinvite": "Invite code copied to clipboard!",
+        "darkinvitecodenotfilled": "You need to fill in the invite code!",
+        "kickedmember": "<strong>{0}</strong> has been kicked from the group!", // {0} is the kicked member name
+        "bannedmember": "<strong>{0}</strong> has been banned from the group!", // {0} is the kicked member name
+        // App store
+        "airplanecantdownload": "Cannot download apps with airplane mode!",
+        // Ads app
+        "adtooshort": "Content is too short!",
+        // Twitter app
+        "replying": "Replying to <strong>@{0}</strong>", // {0} is the tweet's author that a person is replying to.
+        "airplanecantpost": "Cannot post tweets with airplane mode on!",
+        "tweettooshort": "Tweet is too short!",
+        "replytooshort": "Reply is too short!",
+        "nicknametooshort": "Nickname is too short!",
+        "passwordtooshort": "Password is too short!",
+        "settingssaved": "Settings have been saved!",
+        // Bank app
+        "ibancopied": "Your IBAN number has been copied!",
+        "idnotfilled": "You need to fill in the player ID!",
+        "requestidnotfilled": "You need to fill in the player ID!",
+        "amountnotfilled": "You need to fill in the transfer amount!",
+        "requestamountnotfilled": "You need to fill in the request amount!",
+        "purposenotfilled": "You need to specify the transaction reason!",
+        "requestpurposenotfilled": "You need to specify the request reason!",
+        "transfersuccess": "You transfered <strong>{0} €</strong> to player with ID <strong>{1}</strong>!", // {0} is amount {1} is receiver ID
+        "requestsuccess": "You requested <strong>{0} €</strong> from player with ID <strong>{1}</strong>!", // {0} is amount {1} is player ID that ur requesting money from
+        "amounttoolow": "Amount has to be higher than 0 €!",
+        // Choose image
+        "selectanimage": "Please select an image!",
+        "erroruploadingimage": "Error while trying to upload an image!",
+        "uploadinprogress": "An image upload is already in progress!"
+    },
+
+    //////////
+    // HTML //
+    //////////
+    "html": {
+        "just_now": "Just now!",
+        "swipe_to_unlock": "Swipe up to unlock",
+        // Twitter
+        "twitter_title": "Twitter",
+        "twitter_settings_title": "Settings",
+        "change_nickname": "Change nickname",
+        "change_password": "Change password",
+        "change_picture": "Change profile picture",
+        "save_settings": "Save settings",
+        "log_out": "Log out",
+        "twitter_post_title": "Post",
+        "replying": "Replying to <strong>@{0}</strong>", // {0} is the tweet's author that a person is replying to.
+        "post_title": "Title",
+        "post_message": "Message",
+        "post_photo": "Photo",
+        "post_submit": "Submit",
+        "register_title": "Register",
+        "register_description": "Welcome.<br>Sign up to view or post tweets.",
+        "register_rules": `I agree with all <span style="font-family:PoppinsRegular;color: white;">rules.</span>`,
+        "register_button": "Sign up",
+        "already_registered": `Already have an account? <span style="font-family:PoppinsRegular;color: white;" id="loginscreen">Sign in</span>`,
+        "login_title": "Sign in",
+        "login_description": "Welcome.<br>Sign in to view or post tweets.",
+        "login_button": "Sign in",
+        "not_registered": `Don't have an account yet? <span style="font-family:PoppinsRegular;color: white;" id="registerscreen">Sign up</span>`,
+        // Messages
+        "messages_title": "Messages",
+        "new_message_title": "New message",
+        "new_message_name": "Name and Surname",
+        "new_message_phone": "Phone number",
+        "new_message_content": "Text",
+        "new_message_send": "Send",
+        "new_message_cancel": "Cancel",
+        "groups": "GROUPS",
+        "new_group_title": "New group",
+        "new_group_members": "People",
+        "new_group_name": "Name",
+        "new_group_photo": "Photo",
+        "new_group_create": "Create",
+        "manage_group_title": "Manage group",
+        "manage_group_save": "Save Changes",
+        "quick_delete": "Quick Delete",
+        "create_group": "Create group",
+        // App store
+        "featured_app_title": "Featured app",
+        "featured_app_description": "The #1 app to download from app store!",
+        "no_results": "No Results",
+        // Ads
+        "ads_title": `<span style="color: #edce60">AD</span>SPOT`,
+        "ads_description": "Advertisments app",
+        "new_ad_button": "Post",
+        "new_ad_back": "Back",
+        "new_ad_title": "New Advertisment",
+        "new_ad_description": "Ad will be automatically assigned to a category",
+        "new_ad_topic": "Topic",
+        "new_ad_content": "Content",
+        "new_ad_image": "Image",
+        "new_ad_post": "Post",
+        "ad_calls": "Calls",
+        "ad_messages": "Messages",
+        // Contacts
+        "contacts_title": "Contacts",
+        "contacts_foundresults": "TOP NAME MATCHES",
+        "contact_info_title": "Contact",
+        "contact_info_calls": "Call history",
+        "contact_info_clearcalls": "Clear",
+        "contact_edit_title": "Edit",
+        "contact_edit_name": "Name and Surname",
+        "contact_edit_phone": "Phone number",
+        "contact_edit_photo": "Photo",
+        "contact_edit_tag": "Tag",
+        "contact_edit_save": "Save contact",
+        "contact_edit_cancel": "Cancel",
+        "contact_new_title": "Create",
+        "contact_new_name": "Name and Surname",
+        "contact_new_phone": "Phone number",
+        "contact_new_photo": "Photo",
+        "contact_new_tag": "Tag",
+        "contact_new_create": "Create contact",
+        "contact_new_cancel": "Cancel",
+        // Bank
+        "bank_title": "NORDEA",
+        "your_cards": "Korttisi",
+        "choose_card": "Valitse",
+        "balance": "Rahat",
+        "request_money": "Pyydä",
+        "send_money": "Lähetä",
+        "invoices": "Laskut",
+        "phone_transactions": "Tilisiirrot",
+        "transfer": "Siirrä",
+        "transfer_receiver": "Vastaanottajan ID",
+        "transfer_amount": "Summa",
+        "transfer_reason": "Syy",
+        "transfer_submit": "Siirrä",
+        "transfer_cancel": "Peru",
+        "request": "Pyydä",
+        "request_receiver": "Pelaajan ID",
+        "request_amount": "Summa",
+        "request_reason": "Syy",
+        "request_submit": "Pyydä",
+        "request_cancel": "Peruuta",
+        "pay_all_bills": "Maksa kaikki laskut",
+        "invoices_back": "Peruuta",
+        // Dark Chats
+        "darkchat_title": "Anonyymit keskustelut",
+        "darkchat_firstjoin": "Tämä on ensimmäinen kertasi apissa, valitse käyttäjänimi täältä!",
+        "join_chat": "Liity keskusteluun",
+        "dark_chats": "KESKUSTELUT",
+        "darkchatgroupinvite": "Kutsu: <strong>{0}</strong>", // {0} is the invite code
+        "darkchat_newgroup_title": "Uusi ryhmä",
+        "darkchat_newgroup_name": "Nimi",
+        "darkchat_newgroup_members": "Jäsenet",
+        "darkchat_newgroup_photo": "Kuva",
+        "darkchat_newgroup_create": "Luo",
+        "darkchat_editgroup_name": "Nimi",
+        "darkchat_editgroup_members": "Jäsenet",
+        "darkchat_editgroup_photo": "Kuva",
+        "darkchat_editgroup_save": "Tallenna",
+        "darkchat_editgroup_cancel": "Peruuta",
+        'darkchat_editprofile_title': "Muokkaa profiilia",
+        "darkchat_editprofile_name": "Nimi",
+        "darkchat_editprofile_photo": "Kuva",
+        "darkchat_editprofile_save": "Tallena",
+        "darkchat_joingroup_title": "Liity ryhmään",
+        "darkchat_joingroup_invitecode": "Liittymiskoodi",
+        "darkchat_joingroup_join": "Liity",
+        // Notes
+        "notes_title": "Notes",
+        // Phone
+        "phone_title": "Puhelin",
+        "all_calls": "Kaikki",
+        "missed_calls": "Missattu",
+        "dial_title": "Kirjoita numero",
+        "clear_all_calls": "Tyhjennä",
+        // Settings
+        "settings_title": "Asetukset",
+        "backgrounds_title": "Taustakuvat",
+        "ringtones_title": "Soittoäänet",
+        "airplane_mode": "Lentokonetila",
+        "anonymous_call": "Anonyymi puhelu",
+        "airdrop": "AirDrop",
+        "brightness": "Kirkkaus",
+        "sounds": "Äänet",
+        "phone_size": "Puhelimen koko",
+        "group_messaging": "Ryhmäviestit",
+        "background": "Taustakuva",
+        "lock_background": "Lukitusnäytön taustakuva",
+        "ringtones": "Soittoäänet",
+        "background_url": "Taustakuvan URL",
+        "other_backgrounds": "Muut taustakuvat",
+        // Gallery
+        "no_photos": "Ei kuvia!",
+        // Camera
+        "photo_mode": "KUVA",
+        "portrait_mode": "SELFIE",
+        "retake_photo": "Ota uudelleen",
+        "use_photo": "Käytä kuvaa",
+        "cancel": "Peruuta",
+        // Image selection
+        "select_gallery": "Valitse galleriasta",
+        "take_photo": "Ota kuva",
+        "select": "Valitse",
+        // Image preview
+        "save_to_gallery": "Tallenna galleriaan",
+        // AirDrop
+        "airdrop": "AirDrop",
+
+        //////////////////
+        // Placeholders //
+        //////////////////
+        "placeholders": {
+            "nickname": "Käyttäjänimi",
+            "email": "Sähköposti",
+            "password": "Salasana",
+            "image_url": "www.photo.com/photo.png",
+            "tweet_title": "...",
+            "tweet_content": "...",
+            "your_reply": "Vastauksesi",
+            "ad_topic": "Kirjoita tekstisi",
+            "ad_content": "Kirjoita tekstisi",
+            "search_apps": "Etsi appeja",
+            "search_notes": "Etsi muistiinpanoja",
+            "search_contacts": "Etsi kontakteja",
+            "search_chats": "Etsi keskusteluita",
+            "group_name": "...",
+            "chat_message": "Sinun viesti",
+            "enter_nickname": "Kirjoita käyttäjänimesi",
+            "darkchat_group_name": "Ryhmän nimi",
+            "darkchat_group_members": "0",
+            "darkchat_nickname": "...",
+            "darkchat_invitecode": "...",
+            "number_example": "+37060000000",
+            "name_surname": "...",
+            "tag": "...",
+            "player_id": "123",
+            "amount": "0",
+            "reason": "...",
+            "message_content": "..."
+        }
+    }
+}
